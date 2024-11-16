456834cb7b10d859d4c494e98ea607b9122ca2d9
Salih Zioui <www.cjb@gmx.de>
2024-11-16 11:48:55 +0100

    use recommended way of testing with testing library

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,11 @@
 import { describe, expect, it } from "vitest";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { App } from "./App";
 
 describe("App", () => {
 	it("renders App component", () => {
-		const { getByText } = render(<App />);
+		render(<App />);
 
-		expect(getByText("Hello World")).toBeInTheDocument();
+		expect(screen.getByText("Hello World")).toBeInTheDocument();
 	});
 });
